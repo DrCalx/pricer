12b88b1333e6838e6d68f1b584dc34e5b0daba8c
DrCalx <matthew.duguay@gmail.com>
2017-11-28 10:49:18 -0500

    Heroku setup

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const priceRouter = require('./src/routers/price.router')
 
 app.use('/price', priceRouter)
 
-app.listen(3000, () => console.log('Listening on 3000'))
+app.listen(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`))
